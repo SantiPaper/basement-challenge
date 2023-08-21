012830a20d06450f1c0b94f094e40182110c716c
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-08-21 19:23:43 -0300

    Revert "first commit"
    This reverts commit 7c27f8b202e537dff4bd0b8d4319aff0e3678cb9.

--- components/Container/index.tsx
+++
@@ -1,5 +0,0 @@
-import {PropsWithChildren} from "react";
-
-export const Container = ({children}: PropsWithChildren<{}>) => {
-  return <div className="max-w-7xl mx-auto">{children}</div>;
-};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import type {NextPage} from "next";
+import Image from "next/image";
 
-import {Header} from "../components/Header";
+import logo from "../public/logo.svg";
+
 const Home: NextPage = () => {
   return (
-    <>
-      <Header />
-    </>
+    <div className="h-full flex bg-black">
+      <header className="m-auto text-white text-center">
+        <Image alt="Basement" src={logo} />
+        <h4>Lets get this party started</h4>
+      </header>
+    </div>
   );
 };
 
